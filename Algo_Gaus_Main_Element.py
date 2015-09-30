9b8de34aff2b4c7c85f7dda43827302d2b5ef59f
elektro-NIK <bogdan.kalinin@gmail.com>
2015-09-30 20:56:26 +0300

    Create Algo_Gaus_Main_Element.py

---
+++ Algo_Gaus_Main_Element.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def InputA():
+  order = int(raw_input('Введіть порядок матриці: '))
+  print "Матриця:"
+  A = []
+  for i in range(order):
+    row = raw_input().split()
+    for i in range(len(row)):
+      row[i] = float(row[i])
+    A.append(row)
+  for i in range(order):
+    if len(A[i]) != order:
+      print "Матриця не вірна!"
+      exit()
+  return A
+
+def InputB():
+  B = raw_input('Введіть рядок вільних членів: ').split()
+  for i in range(len(B)):
+    B[i] = float(B[i])
+  return B
+
+def AttachMatrixs(A,B):
+  C = []
+  for i in range(len(A)):
+    C.append(A[i])
+    C[i].append(B[i])
+  return C
+
+def MaxElement(C):
+  result = []
+  temp = 0
+  i = 0
+  j = 0
+  while temp == 0:
+    temp = C[i][j]
+    x = j
+    y = i
+    i += 1
+    j += 1
+  for i in range(len(C)):
+    for j in range(len(C)):
+      if C[i][j] > temp:
+        temp = C[i][j]
+        x = j
+        y = i
+  result.append(x)
+  result.append(y)
+  return result
+
+def RemoveRowCol(C, row, col):
+  C1 = C[:row]
+  C2 = C[row+1:]
+  Z = C1 + C2
+  res = []
+  for i in range(len(Z)):
+    res.append(Z[i][:col]+Z[i][col+1:])
+  return res
+
+def MainElement(C):
+  if len(C) == 0:
+    return []
+  temp = MaxElement(C)
+  p = temp[1]
+  q = temp[0]
+  M = []
+  for i in range(len(C)):
+    M.append(-C[i][q]/C[p][q])
+  for i in range(len(C)):
+    if i != p:
+      for j in range(len(C[i])):
+        C[i][j] = C[i][j] + C[p][j]*M[i]
+  X = MainElement(RemoveRowCol(C, p, q))
+  X = X[:q] + [""] + X[q:]
+  S = 0
+  for i in range(len(C[p])-1):
+    if i != q:
+      S += X[i]*C[p][i]
+  X = X[:q] + [(C[p][len(C[p])-1]-S)/C[p][q]] + X[q+1:]
+  return X
+
+print MainElement(AttachMatrixs(InputA(), InputB()))
